b9b90146e3c496ad67e7da4bf8a24d34b291816f
Arsen Abdusalamov <duburlan@gmail.com>
2026-07-24 18:59:22 +0300

    +ftdna fulltree up

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION="1.1.0+2db67e05";
+const APP_VERSION="1.1.0+b2ab8dd0";
 const CACHE='pred-cache-v3';
 const ASSETS=['index.html','corpus.gz','manifest.webmanifest','version.json','predictor-tree.json.gz'];
 const PASSTHRU=/\/(sitemap\.xml|robots\.txt|google[0-9a-f]+\.html)$/;
