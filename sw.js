5268a53da3350a4467949b8cf8ac07c779bff671
Arsen Abdusalamov <duburlan@gmail.com>
2026-07-25 17:04:51 +0300

    topk30 -> topk15 for j1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION="1.1.0+105b9aad";
+const APP_VERSION="1.1.0+08c8d366";
 const CACHE='pred-cache-v3';
 const ASSETS=['index.html','corpus.gz','manifest.webmanifest','version.json','predictor-tree.json.gz'];
 const PASSTHRU=/\/(sitemap\.xml|robots\.txt|google[0-9a-f]+\.html)$/;
