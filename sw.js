c0a334772ed55452ba8a80523c18875fc8028b87
Arsen Abdusalamov <duburlan@gmail.com>
2026-07-27 01:10:59 +0300

    +flags

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION="1.1.1+1bb67a8c";
+const APP_VERSION="1.1.1+37974695";
 const CACHE='pred-cache-v3';
 const ASSETS=['index.html','corpus.gz','manifest.webmanifest','version.json','predictor-tree.json.gz'];
 const PASSTHRU=/\/(sitemap\.xml|robots\.txt|google[0-9a-f]+\.html)$/;
