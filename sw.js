2abfda2c4b7aa1ae9f71ac65ffa355d89c8bcd5d
Arsen Abdusalamov <duburlan@gmail.com>
2026-07-26 23:33:00 +0300

    change algo for nearest

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION="1.1.1+e2739bdb";
+const APP_VERSION="1.1.1+e4c9311c";
 const CACHE='pred-cache-v3';
 const ASSETS=['index.html','corpus.gz','manifest.webmanifest','version.json','predictor-tree.json.gz'];
 const PASSTHRU=/\/(sitemap\.xml|robots\.txt|google[0-9a-f]+\.html)$/;
