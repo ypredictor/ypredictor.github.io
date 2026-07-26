81decd1c262c0796f1f8b1953f9a8365530f9ce3
Arsen Abdusalamov <duburlan@gmail.com>
2026-07-26 10:11:07 +0300

    changelog +version on page

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION="1.1.0+0c8cb396";
+const APP_VERSION="1.1.1+d5431538";
 const CACHE='pred-cache-v3';
 const ASSETS=['index.html','corpus.gz','manifest.webmanifest','version.json','predictor-tree.json.gz'];
 const PASSTHRU=/\/(sitemap\.xml|robots\.txt|google[0-9a-f]+\.html)$/;
