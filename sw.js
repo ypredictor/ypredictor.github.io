43f15efac81701e69b4ef3c14b82756d0adf21b5
Arsen Abdusalamov <duburlan@gmail.com>
2026-08-07 00:22:20 +0300

    predictor: rebuild 1.1.1+2608070022-47676f3a

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION="1.1.1+2608061816-2c9f81a6";
+const APP_VERSION="1.1.1+2608070022-47676f3a";
 const CACHE='pred-cache-v3';
 const ASSETS=['index.html','corpus.gz','manifest.webmanifest','version.json','predictor-tree.json.gz'];
 const PASSTHRU=/\/(sitemap\.xml|robots\.txt|google[0-9a-f]+\.html)$/;
